308e427e95a8dc9084a40d53d48dc3c87681c06d
cadamswaite <cadamswaite@users.noreply.github.com>
2019-09-11 15:24:08 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -21,6 +21,7 @@ apt install openssh-server
 sed -i 's/.*ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/.*PasswordAuthentication .*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/.*UsePAM .*/UsePAM no/g' /etc/ssh/sshd_config
+service ssh start
 service ssh reload
 
 echo "Installing ufw"

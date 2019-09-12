90c3470ccce2bb68393e095aa183ba671e9b3448
cadamswaite <cadamswaite@users.noreply.github.com>
2019-09-12 13:19:37 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -39,7 +39,7 @@ ufw limit ssh/tcp
 echo "Installing fail2ban"
 apt-get -qq install fail2ban -y
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
-sed -i 's/^\[sshd\]/[sshd]\nenabled = true\nfilter = sshd\nport = ssh\nbanaction = iptables-multiport\nbantime = -1\nmaxretry = 3\n/g' /etc/fail2ban/jail.local
+sed -i 's/^\[sshd\]/[sshd]\nenabled = true\nfilter = sshd\nbanaction = iptables-multiport\nbantime = -1\nmaxretry = 3\n/g' /etc/fail2ban/jail.local
 service fail2ban restart
 echo "Fail2ban set up"
 

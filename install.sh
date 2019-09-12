1a931c78169928785f63a3de2a8af931098e3313
cadamswaite <cadamswaite@users.noreply.github.com>
2019-09-12 12:09:40 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -8,21 +8,24 @@ apt-get update && apt-get upgrade -y
 #SSH From https://www.raspberrypi.org/documentation/configuration/security.md
 
 
+apt -qq install openssh-server -y
+service ssh start
+echo "Started ssh server"
+
 if [ -f /home/pi/.ssh/id_rsa ]; then
     echo "id_rsa exists, not generating new ssh key"
 else 
     echo "id_rsa does not exist. Generating ssh key"
     mkdir /home/pi/.ssh/
-    ssh-keygen -t rsa -f /home/pi/.ssh/id_rsa -q -P ""
+    sudo -u pi -g pi -- ssh-keygen -t rsa -f /home/pi/.ssh/id_rsa -q -P ""
+    echo -e "Default \e[31mSSH key generated. Please copy id_rsa to PC now and press enter to continue"
+    read enter
 fi
 
-
-
-apt install openssh-server
+echo "Securing ssh"
 sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/PasswordAuthentication .*/PasswordAuthentication no/g' /etc/ssh/sshd_config
 sed -i 's/UsePAM .*/UsePAM no/g' /etc/ssh/sshd_config
-service ssh start
 service ssh reload
 
 echo "Installing ufw"
@@ -32,7 +35,7 @@ ufw allow ssh
 ufw limit ssh/tcp
 
 echo "Installing fail2ban"
-apt install fail2ban -y
+apt -qq install fail2ban -y
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
 
 # Download bottle for creating webhooks

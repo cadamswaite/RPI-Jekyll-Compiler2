1f39024bbd5b26ec53813b66de49d013968fbe62
cadamswaite <cadamswaite@users.noreply.github.com>
2019-09-12 12:57:42 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -12,19 +12,17 @@ apt-get -qq install openssh-server -y
 service ssh start
 echo "Started ssh server"
 
-if [ -f /home/pi/.ssh/id_rsa ]; then
-    echo "id_rsa exists, not generating new ssh key"
-else 
-    echo "id_rsa does not exist. Generating ssh key"
-    sudo -u pi -g pi -- mkdir /home/pi/.ssh/
-    sudo -u pi -g pi -- ssh-keygen -t rsa -f /home/pi/.ssh/id_rsa -q -P ""
-    mv /home/pi/.ssh/id_rsa.pub /home/pi/.ssh/authorized_keys
-    echo -e "Default \e[31mSSH key generated. Please copy id_rsa to PC now and press enter to continue"
-    read enter
-    rm /home/pi/.ssh/id_rsa
-    chmod 600 /home/pi/.ssh/authorized_keys
-    chmod 600 /home/pi/.ssh/
-fi
+echo "Generating ssh key"
+# Make directory accessible to pi for now, so it can generate keys in that folder.
+sudo -u pi -g pi -- mkdir /home/pi/.ssh/
+sudo -u pi -g pi -- ssh-keygen -t rsa -f /home/pi/.ssh/id_rsa -q -P ""
+mv /home/pi/.ssh/id_rsa.pub /home/pi/.ssh/authorized_keys
+echo -e "Default \e[31mSSH key generated. Please copy id_rsa to PC now and press enter to continue\e[m"
+read enter
+rm /home/pi/.ssh/id_rsa
+chmod 600 /home/pi/.ssh/authorized_keys
+chmod 600 /home/pi/.ssh/
+chown root:root /home/pi/.ssh/
 
 echo "Securing ssh"
 sed -i 's/ChallengeResponseAuthentication .*/ChallengeResponseAuthentication no/g' /etc/ssh/sshd_config
@@ -41,13 +39,8 @@ ufw limit ssh/tcp
 echo "Installing fail2ban"
 apt-get -qq install fail2ban -y
 cp /etc/fail2ban/jail.conf /etc/fail2ban/jail.local
+sed -i 's/^[sshd]/[sshd]\nenabled = true\nfilter = sshd\nport = ssh\nbanaction = iptables-multiport\nbantime = -1\nmaxretry = 3\n/g' /etc/fail2ban/jail.local
+service fail2ban restart
+echo "Fail2ban set up"
 
-# Download bottle for creating webhooks
-echo "Downloading Bottle"
-wget https://bottlepy.org/bottle.py
-
-# Download python script for creating webpages
-sudo ufw allow 80
-ip addr show
-wget https://raw.githubusercontent.com/cadamswaite/RPI-Jekyll-Compiler/master/webhook.py
-python ./webhook.py &
+echo "Install complete!"
